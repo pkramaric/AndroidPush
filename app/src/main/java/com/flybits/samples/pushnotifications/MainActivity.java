e75de694960e84b9b3b7a778ecc704c27e32717a
Petar Kramaric <petar.kramaric@gmail.com>
2016-06-03 18:02:57 -0400

    Completed push notification configuration

--- app/src/main/java/com/flybits/samples/pushnotifications/MainActivity.java
+++ app/src/main/java/com/flybits/samples/pushnotifications/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 login(v);
             }
         });
+        
         btnLocation1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
