ee3d08b93f717a3ac3b231617be977274f5c9bd1
Petar Kramaric <petar.kramaric@gmail.com>
2016-06-03 17:10:51 -0400

    Created PushApplication class and added it to the AndroidManifest.xml file.

---
+++ app/src/main/java/com/flybits/samples/pushnotifications/PushApplication.java
@@ -0,0 +1,22 @@
+package com.flybits.samples.pushnotifications;
+
+import android.app.Application;
+
+import com.flybits.core.api.Flybits;
+import com.flybits.core.api.FlybitsOptions;
+
+public class PushApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        FlybitsOptions builder = new FlybitsOptions.Builder(this)
+                //Additional Options Can Be Added Here
+                .setDebug(true)
+                .enablePushNotifications(FlybitsOptions.GCMType.WITH_GOOGLE_SERVICES_JSON, "")
+                .build();
+
+        //Initialize the FlybitsOptions
+        Flybits.include(this).initialize(builder);
+    }
+
+}
