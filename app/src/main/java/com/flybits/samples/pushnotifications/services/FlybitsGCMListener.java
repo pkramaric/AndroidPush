cab6dded167756058d1ef5ea8daf2026e96d252c
Petar Kramaric <petar.kramaric@gmail.com>
2016-06-05 21:01:25 -0400

    Updated FlybitsGCMListener to accept a push notification, parse it, set a notification in the notification tray, and send it to the MainActivity.

--- app/src/main/java/com/flybits/samples/pushnotifications/services/FlybitsGCMListener.java
+++ app/src/main/java/com/flybits/samples/pushnotifications/services/FlybitsGCMListener.java
@@ -1,30 +1,20 @@
 package com.flybits.samples.pushnotifications.services;
 
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.flybits.core.api.Flybits;
-import com.flybits.core.api.exceptions.FlybitsDisabledException;
-import com.flybits.core.api.exceptions.FlybitsPushException;
-import com.flybits.core.api.interfaces.IRequestCallback;
-import com.flybits.core.api.models.Push;
-import com.flybits.core.api.models.v1_5.internal.Result;
-import com.flybits.core.api.utils.Utilities;
-import com.flybits.core.api.utils.http.GetRequest;
-import com.flybits.samples.pushnotifications.MainActivity;
 import com.flybits.samples.pushnotifications.R;
 import com.google.android.gms.gcm.GcmListenerService;
 import com.google.gson.Gson;
 
-import java.io.IOException;
-
 public class FlybitsGCMListener extends GcmListenerService {
+
+    public final static String MSG_RECEIVED = "PUSH_RECEIVED";
+    public final static String EXTRA_MSG    = "MSG_EXTRA";
 
     public static final String NOTIFICATION_MSG     = "Action.Notification.Msg";
     public static final String NOTIFICATION_HEADER  = "Action.Notification.Header";
@@ -34,97 +24,31 @@ public class FlybitsGCMListener extends GcmListenerService {
     public void onMessageReceived(String from, Bundle data) {
 
         Log.d(TAG, "Received GCMBroadcast: " + data);
-        try {
-            Flybits.include(getBaseContext()).parseGCMPushNotification(data, MomentMessage.class, new IRequestCallback<Push>() {
-                @Override
-                public void onSuccess(Push pushReceived) {
 
-                    if (pushReceived != null && pushReceived.body != null && pushReceived.body instanceof MomentMessage) {
-                        MomentMessage momentMessage = (MomentMessage) pushReceived.body;
-                        new MyTask(getBaseContext()).execute(momentMessage.url);
-                    }
-                }
+        Gson myGson = new Gson();
+        PushBody push  = myGson.fromJson(data.getString("body"), PushBody.class);
 
-                @Override
-                public void onException(Exception e) {
+        Intent broadcastIntent = new Intent(MSG_RECEIVED);
+        broadcastIntent.putExtra(EXTRA_MSG, push.message);
+        sendBroadcast(broadcastIntent);
 
-                }
-
-                @Override
-                public void onFailed(String s) {
-
-                }
-
-                @Override
-                public void onCompleted() {
-
-                }
-            });
-        }catch (FlybitsPushException e){
-            Flybits.include(getBaseContext())._LOG_EXCEPTION(e);
-        }
+        setNotification(getApplicationContext(), "Header Text", push.message);
     }
 
-    static class MomentMessage{
-        public String url;
-        public String zoneId;
-        public String id;
+    public class PushClass{
+        public long version;
+        public long timestamp;
+        public String entity;
+        public String action;
+        public String category;
+        public PushBody body;
     }
 
-    private class MyTask extends AsyncTask<String, Integer, MomentPayload> {
-
-        private Context context;
-
-        public MyTask(Context context){
-            this.context = context;
-        }
-
-        // This is run in a background thread
-        @Override
-        protected MomentPayload doInBackground(String... params) {
-
-            try {
-                Result result = new GetRequest(context, params[0], null).getResponse();
-                if (result.status >= 200 && result.status < 300) {
-
-                    String resultAsString = result.response;
-                    return new Gson().fromJson(resultAsString, MomentPayload.class);
-                }
-            }catch (FlybitsDisabledException | IOException e){
-                return null;
-            }
-
-            return null;
-        }
-
-        // This runs in UI when background thread finishes
-        @Override
-        protected void onPostExecute(MomentPayload result) {
-            super.onPostExecute(result);
-
-            if (result != null && result.localizations != null ){
-                if (result.localizations.en != null ) {
-
-                    try {
-                        String heading = Utilities.hmtlDecodeString(result.localizations.en.name.replace("\n", "<br />"));
-                        String message = Utilities.hmtlDecodeString(result.localizations.en.message.replace("\n", "<br />"));
-                        sendNotification(context, heading, message);
-                    }catch (NullPointerException e){}
-                }
-
-                else if(result.localizations.fr != null){
-
-                    try {
-                        String heading = Utilities.hmtlDecodeString(result.localizations.fr.name.replace("\n", "<br />"));
-                        String message = Utilities.hmtlDecodeString(result.localizations.fr.message.replace("\n", "<br />"));
-                        sendNotification(context, heading, message);
-                    }catch (NullPointerException e){}
-                }
-            }
-        }
+    public class PushBody{
+        public String message;
     }
 
-    private void sendNotification(Context context, String heading, String message) {
+    private void setNotification(Context context, String heading, String message) {
 
         NotificationManager mNotificationManager = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
@@ -135,13 +59,6 @@ public class FlybitsGCMListener extends GcmListenerService {
         //Incase null to avoid crash
         if (message == null)
             message = "";
-
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.putExtra(NOTIFICATION_MSG, message);
-        intent.putExtra(NOTIFICATION_HEADER, heading);
-
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-                intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         long[] vibration = new long[1];
         vibration[0] = 100L;
@@ -155,22 +72,7 @@ public class FlybitsGCMListener extends GcmListenerService {
                         .setAutoCancel(true);
 
 
-        mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(0, mBuilder.build());
     }
 
-    static class MomentPayload{
-        public Localizations localizations;
-    }
-
-    static class Localizations{
-        public Localization en;
-        public Localization fr;
-
-    }
-
-    static class Localization{
-        public String name;
-        public String message;
-    }
 }
