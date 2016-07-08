32b805f10aad1e27a0b21921a97c127ea2c71b65
Petar Kramaric <petar.kramaric@gmail.com>
2016-07-08 11:07:26 -0400

    Added Navigation bar to sample in order to add more feature now and in the future

---
+++ app/src/main/java/com/flybits/samples/pushnotifications/HomeActivity.java
@@ -0,0 +1,219 @@
+package com.flybits.samples.pushnotifications;
+
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.flybits.core.api.Flybits;
+import com.flybits.core.api.interfaces.IRequestCallback;
+import com.flybits.core.api.interfaces.IRequestGeneralCallback;
+import com.flybits.core.api.interfaces.IRequestLoggedIn;
+import com.flybits.core.api.models.User;
+import com.flybits.core.api.utils.filters.LoginOptions;
+import com.flybits.samples.pushnotifications.services.FlybitsGCMListener;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ProgressDialog progressDialog;
+    private ArrayList<String> listOfPushNotifications;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_old);
+
+        progressDialog          = new ProgressDialog(this);
+
+        Button btnLogin             = (Button) findViewById(R.id.btnLogin);
+        Button btnLogout            = (Button) findViewById(R.id.btnLogout);
+        Button btnLaunchPreferences = (Button) findViewById(R.id.btnLaunchPreferences);
+        ListView listView           = (ListView) findViewById(R.id.listOfNotification);
+
+        listOfPushNotifications = new ArrayList<>();
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listOfPushNotifications);
+        listView.setAdapter(adapter);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(v);
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                setProgressBar("Logging Out ...", false);
+
+                Flybits.include(HomeActivity.this).logout(new IRequestGeneralCallback() {
+                    @Override
+                    public void onSuccess() {
+                        Snackbar.make(v, "Successfully Logged Out!", Snackbar.LENGTH_LONG).show();
+
+                        Location location = new Location("yourProvider");
+                        location.setLongitude(43.6495927);
+                        location.setLatitude(-79.3675562);
+                        Flybits.include(HomeActivity.this).updateContextLocation(location, new IRequestGeneralCallback() {
+                            @Override
+                            public void onSuccess() {
+
+                            }
+
+                            @Override
+                            public void onException(Exception e) {
+
+                            }
+
+                            @Override
+                            public void onFailed(String s) {
+
+                            }
+
+                            @Override
+                            public void onCompleted() {
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onException(Exception e) {
+                        Snackbar.make(v, "Something went wrong! ", Snackbar.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailed(String s) {
+
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        stopProgressBar();
+                    }
+                });
+            }
+        });
+
+        btnLaunchPreferences.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                setProgressBar("Confirming Session Status...", false);
+                Flybits.include(HomeActivity.this).isUserLoggedIn(true, new IRequestLoggedIn() {
+                    @Override
+                    public void onLoggedIn(User user) {
+
+                        stopProgressBar();
+                        Intent intent = new Intent(HomeActivity.this, PushPreferencesActivity.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onNotLoggedIn() {
+                        stopProgressBar();
+                        Snackbar.make(v, "Make Sure you are logged in before you continue.", Snackbar.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        IntentFilter filter = new IntentFilter(FlybitsGCMListener.MSG_RECEIVED);
+        registerReceiver(gcmReceiver, filter);
+    }
+
+    BroadcastReceiver gcmReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle extras = intent.getExtras();
+            if (extras != null){
+                String msg = extras.getString(FlybitsGCMListener.EXTRA_MSG);
+                listOfPushNotifications.add(0, msg);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+    public void login(final View v){
+
+        setProgressBar("Logging In...", false);
+        Flybits.include(HomeActivity.this).isUserLoggedIn(true, new IRequestLoggedIn() {
+            @Override
+            public void onLoggedIn(User user) {
+                Snackbar.make(v, "You are already logged in!", Snackbar.LENGTH_LONG).show();
+                stopProgressBar();
+            }
+
+            @Override
+            public void onNotLoggedIn() {
+                LoginOptions options = new LoginOptions.Builder(HomeActivity.this)
+                        .loginAnonymously()
+                        .setRememberMeToken()
+                        .setDeviceOSVersion()
+                        .build();
+
+                Flybits.include(HomeActivity.this).login(options, new IRequestCallback<User>() {
+                    @Override
+                    public void onSuccess(User user) {
+                        Snackbar.make(v, "You have successfully logged in!", Snackbar.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onException(Exception e) {
+                        Snackbar.make(v, "Something went wrong! " + e.getLocalizedMessage(), Snackbar.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailed(String s) {
+
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        stopProgressBar();
+                    }
+                });
+            }
+        });
+    }
+
+    private void setProgressBar(String text, boolean isCancelable) {
+        progressDialog.setCancelable(isCancelable);
+        progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+            public void onCancel(DialogInterface dialog) {
+
+                if (progressDialog.isShowing())
+                    progressDialog.dismiss();
+            }
+        });
+        progressDialog.show();
+        progressDialog.setMessage(text);
+    }
+
+    private void stopProgressBar() {
+        try {
+            if (progressDialog.isShowing())
+                progressDialog.dismiss();
+        } catch (Exception e) {}
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(gcmReceiver);
+        super.onDestroy();
+    }
+}
--- app/src/main/java/com/flybits/samples/pushnotifications/MainActivity.java
+++ app/src/main/java/com/flybits/samples/pushnotifications/MainActivity.java
@@ -1,219 +1,60 @@
 package com.flybits.samples.pushnotifications;
 
-import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-import com.flybits.core.api.Flybits;
-import com.flybits.core.api.interfaces.IRequestCallback;
-import com.flybits.core.api.interfaces.IRequestGeneralCallback;
-import com.flybits.core.api.interfaces.IRequestLoggedIn;
-import com.flybits.core.api.models.User;
-import com.flybits.core.api.utils.filters.LoginOptions;
-import com.flybits.samples.pushnotifications.services.FlybitsGCMListener;
-
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
-    private ProgressDialog progressDialog;
-    private ArrayList<String> listOfPushNotifications;
-    private ArrayAdapter<String> adapter;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        progressDialog          = new ProgressDialog(this);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-        Button btnLogin             = (Button) findViewById(R.id.btnLogin);
-        Button btnLogout            = (Button) findViewById(R.id.btnLogout);
-        Button btnLaunchPreferences = (Button) findViewById(R.id.btnLaunchPreferences);
-        ListView listView           = (ListView) findViewById(R.id.listOfNotification);
-
-        listOfPushNotifications = new ArrayList<>();
-
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listOfPushNotifications);
-        listView.setAdapter(adapter);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                login(v);
-            }
-        });
-
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                setProgressBar("Logging Out ...", false);
-
-                Flybits.include(MainActivity.this).logout(new IRequestGeneralCallback() {
-                    @Override
-                    public void onSuccess() {
-                        Snackbar.make(v, "Successfully Logged Out!", Snackbar.LENGTH_LONG).show();
-
-                        Location location = new Location("yourProvider");
-                        location.setLongitude(43.6495927);
-                        location.setLatitude(-79.3675562);
-                        Flybits.include(MainActivity.this).updateContextLocation(location, new IRequestGeneralCallback() {
-                            @Override
-                            public void onSuccess() {
-
-                            }
-
-                            @Override
-                            public void onException(Exception e) {
-
-                            }
-
-                            @Override
-                            public void onFailed(String s) {
-
-                            }
-
-                            @Override
-                            public void onCompleted() {
-
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onException(Exception e) {
-                        Snackbar.make(v, "Something went wrong! ", Snackbar.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onFailed(String s) {
-
-                    }
-
-                    @Override
-                    public void onCompleted() {
-                        stopProgressBar();
-                    }
-                });
-            }
-        });
-
-        btnLaunchPreferences.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                setProgressBar("Confirming Session Status...", false);
-                Flybits.include(MainActivity.this).isUserLoggedIn(true, new IRequestLoggedIn() {
-                    @Override
-                    public void onLoggedIn(User user) {
-
-                        stopProgressBar();
-                        Intent intent = new Intent(MainActivity.this, PushPreferencesActivity.class);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onNotLoggedIn() {
-                        stopProgressBar();
-                        Snackbar.make(v, "Make Sure you are logged in before you continue.", Snackbar.LENGTH_LONG).show();
-                    }
-                });
-            }
-        });
-
-        IntentFilter filter = new IntentFilter(FlybitsGCMListener.MSG_RECEIVED);
-        registerReceiver(gcmReceiver, filter);
-    }
-
-    BroadcastReceiver gcmReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Bundle extras = intent.getExtras();
-            if (extras != null){
-                String msg = extras.getString(FlybitsGCMListener.EXTRA_MSG);
-                listOfPushNotifications.add(0, msg);
-                adapter.notifyDataSetChanged();
-            }
-        }
-    };
-
-    public void login(final View v){
-
-        setProgressBar("Logging In...", false);
-        Flybits.include(MainActivity.this).isUserLoggedIn(true, new IRequestLoggedIn() {
-            @Override
-            public void onLoggedIn(User user) {
-                Snackbar.make(v, "You are already logged in!", Snackbar.LENGTH_LONG).show();
-                stopProgressBar();
-            }
-
-            @Override
-            public void onNotLoggedIn() {
-                LoginOptions options = new LoginOptions.Builder(MainActivity.this)
-                        .loginAnonymously()
-                        .setRememberMeToken()
-                        .setDeviceOSVersion()
-                        .build();
-
-                Flybits.include(MainActivity.this).login(options, new IRequestCallback<User>() {
-                    @Override
-                    public void onSuccess(User user) {
-                        Snackbar.make(v, "You have successfully logged in!", Snackbar.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onException(Exception e) {
-                        Snackbar.make(v, "Something went wrong! " + e.getLocalizedMessage(), Snackbar.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onFailed(String s) {
-
-                    }
-
-                    @Override
-                    public void onCompleted() {
-                        stopProgressBar();
-                    }
-                });
-            }
-        });
-    }
-
-    private void setProgressBar(String text, boolean isCancelable) {
-        progressDialog.setCancelable(isCancelable);
-        progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-
-            public void onCancel(DialogInterface dialog) {
-
-                if (progressDialog.isShowing())
-                    progressDialog.dismiss();
-            }
-        });
-        progressDialog.show();
-        progressDialog.setMessage(text);
-    }
-
-    private void stopProgressBar() {
-        try {
-            if (progressDialog.isShowing())
-                progressDialog.dismiss();
-        } catch (Exception e) {}
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    protected void onDestroy() {
-        unregisterReceiver(gcmReceiver);
-        super.onDestroy();
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_push) {
+
+        } else if (id == R.id.nav_history) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
