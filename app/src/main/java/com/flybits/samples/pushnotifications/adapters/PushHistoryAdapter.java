4e0aba7de104fe0bf7dffebfcff85a6148be1c22
Petar Kramaric <petar.kramaric@gmail.com>
2016-08-03 15:57:04 -0400

    Updated Push Support for new Alert/Silent notifications

--- app/src/main/java/com/flybits/samples/pushnotifications/adapters/PushHistoryAdapter.java
+++ app/src/main/java/com/flybits/samples/pushnotifications/adapters/PushHistoryAdapter.java
@@ -20,10 +20,12 @@ public class PushHistoryAdapter extends ArrayAdapter<Push> {
     private ArrayList<Push> listOfPushes;
     private LayoutInflater li;
     private DateFormat format;
+    private Context context;
 
     public PushHistoryAdapter(Context context, int resource, ArrayList<Push> objects) {
         super(context, resource, objects);
 
+        this.context        = context;
         listOfPushes        = objects;
         li                  = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         format              = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.CANADA);
@@ -39,19 +41,32 @@ public class PushHistoryAdapter extends ArrayAdapter<Push> {
             convertView             = li.inflate(R.layout.item_push_history, parent, false);
             holder.txtContents      = (TextView) convertView.findViewById(R.id.txtContents);
             holder.txtTimestamp     = (TextView) convertView.findViewById(R.id.txtTimestamp);
+            holder.txtTitle         = (TextView) convertView.findViewById(R.id.txtTitle);
+            holder.txtAlert         = (TextView) convertView.findViewById(R.id.txtAlert);
 
             convertView.setTag(holder);
         }else {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        holder.txtContents.setText(item.bodyAsString);
+        holder.txtContents.setText(context.getString(R.string.txtHistoryCustom, item.bodyAsString));
+
+        int visibilityAlert = (item.alert != null) ? View.VISIBLE : View.GONE;
+        holder.txtAlert.setVisibility(visibilityAlert);
+        holder.txtAlert.setText(context.getString(R.string.txtHistoryAlert, item.alert));
+
+        int visibilityTitle = (item.title != null) ? View.VISIBLE : View.GONE;
+        holder.txtTitle.setVisibility(visibilityTitle);
+        holder.txtTitle.setText(context.getString(R.string.txtHistoryTitle, item.title));
+
         holder.txtTimestamp.setText(format.format(item.timestamp * 1000));
         return convertView;
     }
 
     public static class ViewHolder{
         public TextView txtContents;
+        public TextView txtTitle;
+        public TextView txtAlert;
         public TextView txtTimestamp;
     }
 }
